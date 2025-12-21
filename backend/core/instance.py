30b42645c5e5d41650f965eacc77d3daa7e800d1
tonyo <arabit.tr@gmail.com>
2025-12-21 18:50:18 +0800

    up

--- backend/core/instance.py
+++ backend/core/instance.py
@@ -12,7 +12,7 @@ def create_app():
     setup_logging(app)
 
     # Initialize CORS
-    CORS(app, resources={r"/*": {"origins": app.config['CORS_ORIGINS']}})
+    CORS(app, resources={r"/*": {"origins": app.config['CORS_ORIGINS']}}, supports_credentials=True)
     
     # Initialize extensions here
     jwt = JWTManager(app)
