6dd3d41fc4ccc94da760ceb27391851bbcd88d04
tonyo <arabit.tr@gmail.com>
2025-12-26 10:36:57 +0800

    FLASK_LIMITER: configured environment storage location

--- backend/config.py
+++ backend/config.py
@@ -44,6 +44,9 @@ class Config:
     MAIL_USERNAME = get_config('MAIL_USERNAME', 'mail_username')
     MAIL_PASSWORD = get_config('MAIL_PASSWORD', 'mail_password')
 
+    # Redis (for rate limiting storage)
+    REDIS_URL = get_config('REDIS_URL', default='redis://localhost:6379/0')
+
     # Default Admin Credentials (used if no admin account exists)
     DEFAULT_ADMIN_EMAIL = get_config('DEFAULT_ADMIN_EMAIL', 'default_admin_email')
     DEFAULT_ADMIN_PASSWORD = get_config('DEFAULT_ADMIN_PASSWORD', 'default_admin_password')
